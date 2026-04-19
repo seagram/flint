aa0d01e9aed4981648dadf3770c78fcc50577a84
Cal Seagram <143629409+seagram@users.noreply.github.com>
2026-04-18 21:31:13 -0400

    feat: add clap dependency

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Create,
+    Delete,
+    Edit,
+    List,
+    Restart,
+    Start,
+    Stop,
+}
+
 fn main() {
-    println!("flint");
+    let cli = Cli::parse();
+    match &cli.command {
+        _ => {
+            todo!();
+        }
+    }
 }
